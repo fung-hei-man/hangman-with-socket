46697313441cbd4d6ac0d315260430a0c54cc32f
fung-hei-man <94099670+fung-hei-man@users.noreply.github.com>
2022-10-23 23:06:02 +0800

    feat: multithread client

--- client.py
+++ client.py
@@ -1,27 +1,41 @@
 import socket
+import threading
 
 HOST = 'localhost'
 PORT = 12345
 
 
-def socket_client():
-    try:
-        s = socket.socket()
-        s.connect((HOST, PORT))
-    except socket.error as e:
-        print('Oops! Something wrong when connecting to server')
-        print(str(e))
-        return
+class MultiThreadClient(threading.Thread):
+    def __init__(self, client_idx):
+        super().__init__()
+        self.client_idx = client_idx
 
-    data = s.recv(1024).decode()
-    print(f'> Date from server: {data}')
+    def run(self):
+        try:
+            s = socket.socket()
+            s.connect((HOST, PORT))
+        except socket.error as e:
+            print('Oops! Something wrong when connecting to server')
+            print(str(e))
+            return
 
-    s.send(str.encode('Hello server'))
-    data = s.recv(1024).decode()
-    print(f'> Date from server: {data}')
+        data = s.recv(1024).decode()
+        print(f'> Date from server: {data}')
 
-    s.close()
+        s.send(str.encode(f'Hello server, I am client #{self.client_idx}'))
+        data = s.recv(1024).decode()
+        print(f'> Date from server: {data}')
+
+        s.close()
 
 
 if __name__ == '__main__':
-    socket_client()
+    client_num = input('Please input number of clients needed (default=5): ')
+    try:
+        client_num = int(client_num)
+    except ValueError:
+        client_num = 5
+
+    for i in range(1, client_num+1):
+        client = MultiThreadClient(i)
+        client.start()
